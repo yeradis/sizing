f473d165702c18bd2e3b80348c1731727ba7fa1b
Yeradis P. Barbosa Marrero <yeradis@gmail.com>
2013-11-07 00:23:41 +0400

    using memcached through nginx

--- sizing.py
+++ sizing.py
@@ -1,19 +1,29 @@
 # -*- coding: utf-8 -*-
 ## libpng,libjpeg for mac http://ethan.tira-thompson.com/Mac_OS_X_Ports.html
 from flask import Flask, request, make_response, send_file
-from PIL import Image
+from PIL import Image,ImageOps
 from flask.ext.cache import Cache
 
 import requests
 from StringIO import StringIO
+import os
 
-cache = Cache()
+import Queue
+import threading
+
+os.environ['http_proxy']=''
+requests.adapters.DEFAULT_RETRIES = 5
+
 app = Flask(__name__)
-cache.init_app(app, config={'CACHE_TYPE': 'simple'})
 
-@cache.memoize(50)
+q = Queue.Queue()
+def put_url(q, url, body):
+	q.put(requests.put(url,data=body).close())
+
 def serve_image(img_source_url,new_width,quality):
-	img = Image.open(StringIO(requests.get(img_source_url).content))
+	req = requests.get(img_source_url, verify=False)
+	img = Image.open(StringIO(req.content))
+	req.close()
 	format = img.format
 	percent = (new_width/float(img.size[0]))
 	new_height = int((float(img.size[1])*float(percent)))
@@ -21,15 +31,41 @@ def serve_image(img_source_url,new_width,quality):
 	img_io = StringIO()
 	img.save(img_io,"%s"%(format),quality=quality)
 	img_io.seek(0)
+	
+	#storing into memcached through nginx
+	try:
+		url = request.url
+		body = img_io.getvalue()
+		t = threading.Thread(target=put_url, args = (q,url,body))
+		t.daemon = True
+		t.start()
+	except ValueError:
+		print 'something wrong'
+		print ValueError
+	
+	return send_file(img_io,mimetype='IMAGE/%s'%(format),as_attachment=False)
+
+def fit_image(img_source_url,new_width,quality):
+	img = Image.open(StringIO(requests.get(img_source_url).content))
+	format = img.format
+	percent = (new_width/float(img.size[0]))
+	new_height = int((float(img.size[1])*float(percent)))
+	img = ImageOps.fit(img, (new_width, new_height), Image.ANTIALIAS)
+	img_io = StringIO()
+	img.save(img_io,"%s"%(format),quality=quality)
+	img_io.seek(0)
 	return send_file(img_io,mimetype='IMAGE/%s'%(format),as_attachment=False)
 
 @app.route('/sizing/<int:quality>/<int:width>/<path:url>')
 def sizing(quality,width,url):
-	try:
-		return serve_image(url,width,quality)	
-	except:
-		return "<h1>500 - Internal Server Error</h1>", 500
+	return serve_image(url,width,quality)
 	
+@app.route('/sizing/fit/<int:quality>/<int:width>/<path:url>')
+def sizing_fit(quality,width,url):
+        try:
+                return fit_image(url,width,quality)
+        except:
+                return "<h1>500 - Internal Server Error</h1>", 500
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=5000)
+    app.run(threaded=True, debug=True,host='0.0.0.0', port=5002)
